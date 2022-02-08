7f3d0ee7de1ebe471a99d21b70281f7c130fbb68
AndrewSimons3 <simdrew07@gmail.com>
2022-02-08 09:44:12 -0600

    updated html semantic issues

--- src/components/FeatureItem.js
+++ src/components/FeatureItem.js
@@ -5,11 +5,11 @@ const FeatureItem = (props) => {
   return (
 	
 				<div className={classes.feature}>
-					<h3>{`${props.title}`}</h3>
+					<h2>{`${props.title}`}</h2>
 					<p>{props.description}</p>
-					<button>
+					
 						<a href='{props.link}'>Learn More</a>
-					</button>
+					
 				</div>
 		
 	);
